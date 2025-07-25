a2dd93a78fcf4604db0e8307d92f804384268d06
Sumit Mangal <sumit12690@gmail.com>
2025-07-25 09:45:04 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package.json
    	new file:   pnpm-lock.yaml
    	modified:   src/App.css
    	modified:   src/App.jsx
    	modified:   src/index.css
    	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // or vue, or other framework plugin
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(), // or your framework plugin
+    tailwindcss(),
+  ],
+});
